e3ab7b0012557c956067e0736e66cc3da455cb06
“Sujitmore21” <Sujitmore21@gmail.com>
2024-06-14 13:09:29 +0530

    Add dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,18 @@
-import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/core/theme/theme.dart';
-import 'package:blog_app/festures/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:blog_app/festures/auth/data/repositories/auth_repository_impl.dart';
-import 'package:blog_app/festures/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/festures/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/festures/auth/presentation/pages/login_page.dart';
+import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
-  // ignore: unused_local_variable
-  final supabase = await Supabase.initialize(
-    url: AppSecrets.supabaseUrl,
-    anonKey: AppSecrets.supabaseAnonKey,
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependencies();
   runApp(
     MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (_) => AuthBloc(
-            userSignUp: UserSignUp(
-              AuthRepositoryImpl(
-                AuthRemoteDataSourceImpl(
-                  supabase.client,
-                ),
-              ),
-            ),
-          ),
+          create: (_) => serviceLocator<AuthBloc>(),
         ),
       ],
       child: const MyApp(),
@@ -38,7 +23,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
